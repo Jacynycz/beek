09200f67e269c150f6552e9b12b973dc0ef9a1c8
Viktor Jacynycz <viktor@jacynycz.es>
2017-05-18 15:35:07 +0200

    test

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if command -v hostapd >/dev/null 2>&1 ; then
+    echo "hostapd found"
+else
+    echo "hostapd not found, please install hostapd"
+fi
+
+if command -v isc-dhcp-server >/dev/null 2>&1 ; then
+    echo "isc-dhcp-server found"
+else
+    echo "isc-dhcp-server not found, please install isc-dhcp-server"
+fi
+
+
+
+# write out cron demon for checking the 
+# mac addresses connected to the router
+#crontab -l > mycron
+#echo "* * * * * $PWD/check_trusted.sh" >> mycron
+#crontab mycron
+#rm mycron
